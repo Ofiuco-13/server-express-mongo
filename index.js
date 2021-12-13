e48002ae652b35621172c41da3210d9c6d5d0ccb
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-12-12 22:58:25 -0300

    Descarga dependencias de proyecto

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hola Mundo");
-})
+//Settings
+app.set("appName", "Pharmaceutical MERN");
+app.set("port", 3000);
 
-app.listen(5000);
+//Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//Rutas
+app.get("/", (req, res) => {
+  res.send("Hola Mundo");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("appName"));
+  console.log("Servidor funcionando en puerto", app.get("port"));
+});
