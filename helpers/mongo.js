d72cb645b4633ad322a9629a32332e82c2848bb0
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-12-14 22:00:15 -0300

    crea funcion asincrona

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -5,4 +5,11 @@ const url = "mongodb://localhost:27017";
 const cliente = new MongoClient(url);
 
 //Definiendo nombre de base de datos
-const nombreDB = "miProyecto";
+const nombreDB = "express-server";
+
+async function main() {
+    //Use metodo connect para conectar con el servidor
+    await cliente.connect();
+    console.log("Conexion exitosa al servidor, no hay problemas");
+    const db =cliente.db(nombreDB);
+}
