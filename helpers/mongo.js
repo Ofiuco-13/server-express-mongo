38d6a9a8db2fd17a93a9f82c54dc06c131cd9c17
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-12-14 22:50:14 -0300

    Configura quick start

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -7,9 +7,23 @@ const cliente = new MongoClient(url);
 //Definiendo nombre de base de datos
 const nombreDB = "express-server";
 
+
+//async es para delcarar que el codigo serae asincrono, es decir, la secuencia no va a seguir sin que await se complete
 async function main() {
-    //Use metodo connect para conectar con el servidor
-    await cliente.connect();
-    console.log("Conexion exitosa al servidor, no hay problemas");
-    const db =cliente.db(nombreDB);
+  //Use metodo connect para conectar con el servidor
+  await cliente.connect();
+  //Solamente cuando la conexion al cliente sea exitosa se ejectura lo siguiente:
+  console.log("Conexion exitosa al servidor, no hay problemas");
+  //Se conectara (servidor) a la base de datos que esta definida como nombreDB
+  const db = cliente.db(nombreDB);
+
+  const coleccion = nombreDB.coleccion("documentos");
+
+  return "Hechp.";
 }
+
+//llama a la funcion main()
+main()
+   .then(console.log);
+   .catch(console.error);
+   .finally() => cliente.close();
